461688afb744a7b0a3899f6f69b7d1f1ba2c1dd1
Ruslan <ruslanfilistovich@gmail.com>
2020-04-13 17:11:38 +0300

    learn kvo and kvc

--- KVO_KVC/ViewController.swift
+++ KVO_KVC/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Person: NSObject {
+    @objc dynamic var name = String()
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @objc let ruslan = Person()
+    @objc dynamic var inputText: String?
+    
+    var nameObservation: NSKeyValueObservation?
+    var inputTextObservation: NSKeyValueObservation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameObservation = observe(\ViewController.ruslan.name, options: [.new, .old]) { (vc, change) in
+            guard let updateName = change.newValue else { return }
+            self.nameLabel.text = updateName
+            print("Изменили старое значение:\(change.oldValue) на новое - \(change.newValue)")
+        }
+        
+        inputTextObservation = observe(\ViewController.inputText, options: [.new]) { (vc, change) in
+            guard let updatedInputText = change.newValue as? String else { return }
+            vc.textLabel.text = updatedInputText
+        }
+        
     }
-
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        ruslan.name = "Poman"
+    }
+    
+    @IBAction func textFieldEditingChanget(_ sender: Any) {
+        inputText = textField.text
+    }
 
 }
 
