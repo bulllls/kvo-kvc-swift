a7c8ea424a155a9a4d3b2ba5a4134705fe086168
Ruslan <ruslanfilistovich@gmail.com>
2020-04-13 10:10:31 +0300

    Initial Commit

---
+++ KVO_KVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVO_KVC
+//
+//  Created by Ruslan on 4/13/20.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
